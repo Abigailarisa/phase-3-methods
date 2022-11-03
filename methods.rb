d91c884cd831ec02deb108274d3d9cdad9789374
Abigail Arisa <110755503+Abigailarisa@users.noreply.github.com>
2022-11-02 22:45:25 -0500

    commit tree 3c15631d006af5c90fa05ebdb77b48ec2f82e8ca

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+     num1 + num2
+end
+# num1 = 1
+# num2 = 2
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
